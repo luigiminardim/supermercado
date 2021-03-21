ff5bc5a79aef5fe7d95830555e990f377d68685a
Luigi Minardi <luigiminardim@gmail.com>
2021-03-21 16:59:05 -0300

    New folder organization.

--- src/components/shared/Header/components/MobileHeader/index.tsx
+++
@@ -1,8 +0,0 @@
-import { Flex } from "@chakra-ui/layout";
-import { HTMLChakraProps } from "@chakra-ui/system";
-
-export function MobileHeader({
-  ...props
-}: HTMLChakraProps<"div">): JSX.Element {
-  return <Flex direction="row" bg="primary" h="10" {...props}></Flex>;
-}
--- src/components/shared/Header/index.tsx
+++ src/components/shared/Header/index.tsx
@@ -1,11 +1,10 @@
 import { useBreakpointValue } from "@chakra-ui/media-query";
 import { DesktopHeader } from "./components/DesktopHeader";
-import { MobileHeader } from "./components/MobileHeader";
 
 export function Header() {
   return (
     <>
-      <DesktopHeader/>
+      <DesktopHeader />
     </>
   );
 }
