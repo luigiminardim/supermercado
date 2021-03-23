b42ae73371da86c08d64d701f23acb21a33b477e
Luigi Minardi <luigiminardim@gmail.com>
2021-03-23 20:03:56 -0300

    some bug fixed.

--- src/pages/products/[slug].tsx
+++ src/pages/products/[slug].tsx
@@ -1,4 +1,5 @@
 import { GetStaticPaths, GetStaticProps } from "next";
+import { useRouter } from "next/router";
 import { useEffect } from "react";
 import {
   ProductScreen,
@@ -6,37 +7,35 @@ import {
 } from "../../components/screens/ProductScreen";
 
 export default function ProductPage({ item }: ProductScreenProps): JSX.Element {
+  const router = useRouter();
+  if (router.isFallback) {
+    return <></>;
+  }
+
   return <ProductScreen item={item} />;
 }
 
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    fallback: true,
+    paths: [],
+  };
+};
+
 export const getStaticProps: GetStaticProps = async ({ params: { slug } }) => {
-  if (slug === "") {
-    return {
-      redirect: {
-        destination: "/",
-        permanent: true,
-      },
-    };
-  }
+  console.log("slug: ", slug);
 
   const response = await fetch(
     `https://api.instabuy.com.br/apiv3/item/?subdomain=supermercado&slug=${slug}`
   );
-
+  const data = await response.json();
   const {
     data: [item],
-  } = await response.json();
+  } = data;
 
   const $1day = 24 * 60 * 60;
   return {
     props: { item },
-    revalidate: $1day,
-  };
-};
-
-export const getStaticPaths: GetStaticPaths = async () => {
-  return {
-    paths: [],
-    fallback: true,
+    revalidate: $1day
   };
 };
